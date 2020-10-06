1974cfa1a8457ebaeb192a6bb204d3689d58e4c9
lacasti <64714511+lacasti@users.noreply.github.com>
2020-10-06 08:27:42 -0500

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-        System.out.println("Hello, World from Microservices ..uu");
+        System.out.println("Hello, World from Microservices ..uu!!!");
   }
 }
